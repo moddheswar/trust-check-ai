8785bda6ef66b1e13ebb102ada5a687e20c59e5e
moddheswar <smoddheswar@gmail.com>
2026-01-15 12:41:35 +0530

    pre-process_text

---
+++ utils/text-preprocessing.py
@@ -0,0 +1,15 @@
+import string
+
+
+def preprocess_texts(statements):
+    processed_list = []
+    for text in statements:
+        text = text.lower()
+        text = text.translate(str.maketrans('', '', string.punctuation))
+        tokens = text.split()
+        processed_list.append(tokens)
+    return processed_list
+
+
+
+
